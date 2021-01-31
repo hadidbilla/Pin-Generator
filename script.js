5333ba7b50aa8b7b6df6af5eb15a11c5df05f1d6
hbhridoy449 <50765387+hbhridoy449@users.noreply.github.com>
2021-02-01 02:02:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function generateNumber() {
+  let randomNumber = parseInt(Math.random() * 10000);
+  if (randomNumber <= 999) {
+    generateNumber();
+  } else {
+    document.getElementById("random-input").value = randomNumber + "";
+  }
+}
+function handelClickNumber(number) {
+  let clickNumber = (document.getElementById("match-input").value +=
+    number + "");
+}
+function matchTwoNumber() {
+  let firstNumber = document.getElementById("random-input").value;
+  let secondNumber = document.getElementById("match-input").value;
+  if (firstNumber == "" && secondNumber == "") {
+    document.getElementById("pls-write").style.display = "block";
+  } else if (firstNumber == secondNumber) {
+    document.getElementById("yes-match").style.display = "block";
+  } else {
+    document.getElementById("not-match").style.display = "block";
+  }
+}
